a8962e09d39190004d9dc464b3956f1acacd75b5
Daniil <42042452+xxPFFxx@users.noreply.github.com>
2019-10-17 20:27:28 +0300

    Реализовал Ajax

--- ajax.php
+++ ajax.php
@@ -1,3 +1,9 @@
 <?php
-if (isset($_GET['y'])) echo $_GET['y']
+if (isset($_GET['y'])) $y = $_GET['y'];
+$y = preg_replace("/,/", ".", $y);
+$fail = "";
+if (preg_match("/[^-\d,.]/", $y)) $fail.= "Введены недопустимые символы\n";
+elseif ($y == "" || !is_numeric($y)) $fail .= "Y должен быть числом\n";
+elseif ($y<-3 || $y>5) $fail.= "Y должен находиться от -3 до 5";
+echo $fail;
 ?>
